0ac554dac6e810cda28f1f38b763ddeb2ebbeff4
3omd4 <mohamedemadeldin38@gmail.com>
2025-02-01 23:05:11 +0200

    done

---
+++ finding-lanes/lanes.py
@@ -0,0 +1,5 @@
+import cv2
+
+image= cv2.imread('test_image.jpg')
+cv2.imshow('result', image)#this function should be followed by the waitkey()
+cv2.waitKey(0)#this function display the image for the specified time in ms 
